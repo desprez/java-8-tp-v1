da722eab3cbada7e85346bae9181f9cc7a71450d
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-10-01 23:18:33 +0200

    rename packages

--- src/main/java/com/bnpparibas/training/lambda/tp1/MathOperation.java
+++
@@ -1,8 +0,0 @@
-package com.bnpparibas.training.lambda.tp1;
-
-@FunctionalInterface
-public interface MathOperation {
-
-	int sommer(int a, int b);
-
-}
--- src/main/java/com/bnpparibas/training/methodreferences/tp2/StringListFormatter.java
+++
@@ -1,8 +0,0 @@
-package com.bnpparibas.training.methodreferences.tp2;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface StringListFormatter {
-	String format(String delimiter, List<String> list);
-}
