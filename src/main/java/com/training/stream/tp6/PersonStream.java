354cae460a4f7239ef0692a8fa6c6d834443ec76
pascal.desprez <pascal.desprez@fr.ibm.com>
2021-02-10 14:31:29 +0100

    add comments

--- src/main/java/com/training/stream/tp6/PersonStream.java
+++ src/main/java/com/training/stream/tp6/PersonStream.java
@@ -3,10 +3,22 @@ package com.training.stream.tp6;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Exercice :
+ *
+ * - Trouver les personnes qui ont un nom Jeff
+ *
+ * - Trouver l'âge maximal des personnes
+ *
+ * - Convertir la liste de personnes en une Map avec comme clé l'âge et la
+ * valeur le nom de la personne : List<Person> vers Map<Integer, String>
+ *
+ * - Afficher tous les éléments de la Map en passant par les API java 8.
+ */
 public class PersonStream {
 
-	public static void main(String[] args) {
-		List<Person> persons = Arrays.asList(new Person("Brian", 22), new Person("Jeff", 20), new Person("Jimmy", 40));
+	public static void main(final String[] args) {
+		final List<Person> persons = Arrays.asList(new Person("Brian", 22), new Person("Jeff", 20), new Person("Jimmy", 40));
 	}
 
 }
