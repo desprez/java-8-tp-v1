23b9e7b1c7fbb437b0ae31234e144e4583d58f2c
pascal.desprez <pascal.desprez@fr.ibm.com>
2021-02-10 22:44:06 +0100

    add comments

--- src/main/java/com/training/datetime/tp1/CalculDuration.java
+++ src/main/java/com/training/datetime/tp1/CalculDuration.java
@@ -1,12 +1,19 @@
 package com.training.datetime.tp1;
 
+/**
+ * Exercice :
+ *
+ * Convertir le calcul de la durée d’exécution de la classe en passant par les
+ * nouvelles API Java 8 : Instant & Duration
+ *
+ */
 public class CalculDuration {
 
-	public static void main(String[] args) throws InterruptedException {
-		long start = System.currentTimeMillis();
+	public static void main(final String[] args) throws InterruptedException {
+		final long start = System.currentTimeMillis();
 		Thread.sleep(5000);
-		long end = System.currentTimeMillis();
-		System.out.println("Took : " + ((end - start) / 1000) + " sec");
+		final long end = System.currentTimeMillis();
+		System.out.println("Took : " + (end - start) / 1000 + " sec");
 	}
 
 }
